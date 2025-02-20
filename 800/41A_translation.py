5cd8efcae366e7a48d13ef5a0486c13ae52359c7
Ethan Vieira <ethanvieira@gmail.com>
2025-02-20 09:07:30 -0600

    add 41A

---
+++ 800/41A_translation.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+def input_int():
+    return(int(input()))
+
+def input_list():
+    return(list(map(int,input().split())))
+
+def input_list_str():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+def input_str():
+    return input().rstrip()
+
+def input_multiple_ints():
+    return(map(int,input().split()))
+
+def main():
+    s = input_str()
+    s2 = input_str()
+
+    output("YES" if s2 == s[::-1] else "NO")
+
+if __name__ == "__main__":
+    main()
