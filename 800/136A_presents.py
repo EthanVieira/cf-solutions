27bb082ee75ae655a48336ae1d8bd6c890d10465
Ethan Vieira <ethanvieira@gmail.com>
2025-02-24 08:26:57 -0600

    Create 136A_presents.py

---
+++ 800/136A_presents.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+def input_int():
+    return(int(input()))
+
+def input_list():
+    return(list(map(int,input().split())))
+
+def input_list_str():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+def input_str():
+    return input().rstrip()
+
+def input_multiple_ints():
+    return(map(int,input().split()))
+
+def main():
+    n = input_int()
+    ns = input_list()
+    result = [0] * n
+    for i, x in enumerate(ns):
+        result[x - 1] = i + 1
+    output(" ".join([str(x) for x in result]))
+
+if __name__ == "__main__":
+    main()
