00638fa21d915c84b0841fa5a327ce4d2d2f682a
Ethan Vieira <ethanvieira@gmail.com>
2025-03-01 12:37:51 -0600

    Create 133A_hq9.py

---
+++ 900/133A_hq9.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+def input_int():
+    return(int(input()))
+
+def input_list():
+    return(list(map(int,input().split())))
+
+def input_list_str():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+def input_str():
+    return input().rstrip()
+
+def input_multiple_ints():
+    return(map(int,input().split()))
+
+def main():
+    s = input_str()
+    output("YES" if any((c in set('HQ9')) for c in s) else "NO")
+
+if __name__ == "__main__":
+    main()
